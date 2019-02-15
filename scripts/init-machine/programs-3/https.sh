20a66f6faf885f7059faaee7b6b1d24e809e0a8b
jammygit <mail@nathanschuetz.ca>
2019-02-14 20:51:28 -0700

    fix login bug and add more comments

--- scripts/init-machine/programs-3/https.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo 'https NOT configured!'
-
-echo 'todo - get a singe cert, store on s3, and have all new servers GET is \
-from there'
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // libraries (alphabetic)
-const bcrypt = require('bcryptjs');                 // cryptographic hashing algorithm for passwords
-const body_parser = require('body-parser');         // to give us access to body of http requests
+const bcrypt = require('bcryptjs'); // cryptographic hashing algorithm for passwords
+const body_parser = require('body-parser'); // to give us access to body of http (post) requests
 const client_sessions = require('client-sessions'); // mozilla library to implement encrypted client-side sessions
 const express = require('express');
 
@@ -11,13 +11,14 @@ const express = require('express');
 // ****************************************************************** some setup
 const app = express();
 app.set('view engine', 'ejs');
-app.set('views', `${__dirname}/views`);          // says 'our ejs is here'
-app.use('/static', express.static(`${__dirname}/static`));  // sort of automatic route handling for this directory
+app.set('views', `${__dirname}/views`); // says 'our ejs is here'
+// says 'our static files live here', ie, most image files we will serve
+app.use('/static', express.static(`${__dirname}/static`));
 
 
 
 // ****************************************************************** middleware
-// to get request bodies in object form
+// if a http request has a 'body,' this will add it to our request object
 app.use(body_parser.urlencoded({
     extended: false
 }));
@@ -25,36 +26,34 @@ app.use(body_parser.urlencoded({
 // set up client-side sessions
 app.use(client_sessions({
     cookieName: 'session',
-    secret: process.env.SESSION_SECRET,    // basically a symmetric encryption key
-    duration: 1000 * 60 * 30,               // 30 minutes
+    secret: process.env.SESSION_SECRET, // basically a symmetric encryption key
+    duration: 1000 * 60 * 30, // 30 minutes
     activeDuration: 1000 * 60 * 5,
     cookie: {
         path: '/',
         httpOnly: true,
         sameSite: true,
-        secureProxy: !process.env.IS_DEV,
+        secureProxy: !process.env.IS_DEV, // only relevant for https
         ephemeral: true
     }
 }));
 
-// lookup user ahead of time, must be after client_sessions
+// if user has an active session, attach a 'user' object to the request object
 app.use((req, res, next) => {
     // if session does not exist, it was not found above by client_sessions.
     if (!req.session) {
         return next();
     } else {
-        // typical step to put some user info on the request object. Stubbed out.
+        // simulate adding user data to the request object. Just a stub
         req.user = 'timmay';
         return next();
     }
 });
 
-// confirms that login was verified above
+// used in routes requiring login: if not logged in, redirect to /login
 function login_required(req, res, next) {
-    if (!req.session.user_id) {
-        return res.redirect('/login');
-    }
-
+    // since a real sesion has a user_id attached at login time, this verifies login
+    if (!req.session.user_id) return res.redirect('/login');
     return next();
 }
 
@@ -62,13 +61,23 @@ function login_required(req, res, next) {
 
 // ********************************************************************** routes
 app.get('/login', (req, res) => {
-    req.session.user_id = 123;
     res.render('login');
 });
 
 app.post('/login', (req, res) => {
+    // simulate a password hash performed at login time.
+    // note: does not actually compare passwords
     !bcrypt.compareSync(req.body.password, 'password');
+    // client-sessions created a session, this adds some user data to it
+    req.session.user_id = 123;
     return res.redirect('/dashboard');
+});
+
+// mostly to reset state between fake user sessions
+router.get('/logout', (req, res) => {
+    req.session.reset();
+    res.clearCookie("session");
+    res.redirect('/');
 });
 
 app.get('/dashboard', login_required, (req, res) => {
